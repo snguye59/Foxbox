904a3512c42a78650055688f536edbf75fc89097
Son <gamerboixyz123@gmail.com>
2026-02-03 14:30:29 -0600

    Fix header data links

--- site/src/data/headerData.js
+++ site/src/data/headerData.js
@@ -1,11 +1,11 @@
 export const headerData = [
-  { name: "Products", link: "/", icon: "box" },
-  { name: "Developers", link: "/", icon: "cube" },
-  { name: "Pricing", link: "/", icon: "shoppingCart" },
-  { name: "Security", link: "/", icon: "lock" },
-  { name: "Support", link: "/", icon: "chatBubbles" },
-  { name: "Blog", link: "/", icon: "pen" },
-  { name: "Sign in", link: "/", icon: "bracketArrow", isAuthLink: true },
+  { name: "Products", link: "/products", icon: "box" },
+  { name: "Developers", link: "/developers", icon: "cube" },
+  { name: "Pricing", link: "/pricing", icon: "shoppingCart" },
+  { name: "Security", link: "/security", icon: "lock" },
+  { name: "Support", link: "/support", icon: "chatBubbles" },
+  { name: "Blog", link: "/blog", icon: "pen" },
+  { name: "Sign in", link: "/login", icon: "bracketArrow", isAuthLink: true },
   {
     name: "Register",
     link: "/register",
